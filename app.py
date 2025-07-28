af0a56d8ba54a346cb3159711b83acb03a932634
Desalegn <deribedesalegn@gmail.com>
2025-07-28 04:46:43 +0300

    adding to database and rendering it

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
 #imports 
-from flask import Flask
+from flask import Flask, render_template,redirect,request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 #My app
 app=Flask(__name__)
+Scss(app)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///database.db"
+db=SQLAlchemy(app)
+
+#Data class- row of data
+class MyTask(db.Model):
+   id=db.Column(db.Integer, primary_key=True)
+   content=db.Column(db.String(100) ,nullable=False)
+   completed=db.Column(db.Integer, default=0)
+   created=db.Column(db.DateTime, default=datetime.utcnow)
+
+   def __repr__(self) -> str:
+      return f"Task{self.id}"
 
 
-@app.route("/")
+
+@app.route("/" ,methods=["POST","GET"])
 def index():
-    return "This is Desalegn Deribe"
+   if request.method=="POST":
+      current_task=request.form['content']
+      new_task=MyTask(content=current_task)
+      try:
+         db.session.add(new_task)
+         db.session.commit()
+         return redirect("/")
+      except Exception as e:
+         print(f"ERROR:{e}")
+         return f"ERROR:{e}"
+      #see all current tasks
+   else:
+    tasks=MyTask.query.order_by(MyTask.created).all()
+    return render_template('index.html', tasks=tasks)
+
+
+
+
 
 if __name__ == "__main__":
+  with app.app_context():
+     db.create_all()
   app.run(debug=True)
